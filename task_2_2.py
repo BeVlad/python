ce3518a8ecaf3346bf7b97f3e3923747cc1367c7
Vlad <110487679+BeVlad@users.noreply.github.com>
2022-08-26 23:48:19 +0700

    Update task_2_2.py

--- task_2_2.py
+++ task_2_2.py
@@ -1,12 +1,8 @@
-elem_count = int(input("Please, enter the number of list items: "))
-test_list = []
+list_elem = input("Please, enter the number of list items: ").split(' ')
 i = 0
-elem = 0
-while i < elem_count:
-    test_list.append(input("Please, enter the following value: "))
-    i += 1
-
-for elem in range(int(len(test_list)/2)):
-        test_list[elem], test_list[elem + 1] = test_list [elem + 1], test_list[elem]
-        elem += 2
-print(test_list)
+j = 1
+while len(list_elem) > j:
+    list_elem[i], list_elem[j] = list_elem[j], list_elem[i]
+    i += 2
+    j += 2
+print('Your digits: ', *list_elem)
