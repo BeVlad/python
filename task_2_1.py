94a4eb0d15482ebcdac3b7cfa3bad784d4d017f8
Vlad <110487679+BeVlad@users.noreply.github.com>
2022-08-26 23:48:48 +0700

    Update task_2_1.py

--- task_2_1.py
+++ task_2_1.py
@@ -1,6 +1,3 @@
 type_list = [12, None, -77, 'True', True, 9.5]
-def test_type(el):
-    for el in range(len(type_list)):
-        print(type(type_list[el]))
-    return
-test_type(type_list)
+for el in type_list:
+    print(type(el))
