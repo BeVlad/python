ee627c23d9f32f2201199e18ee5c0b7b4c5fa2ac
Vlad <110487679+BeVlad@users.noreply.github.com>
2022-08-26 23:49:16 +0700

    Update task_2_3.py

--- task_2_3.py
+++ task_2_3.py
@@ -4,13 +4,13 @@ if month == 1 or month == 12 or month == 2:
         print(seasons.get(1))
 
 elif month == 3 or month == 4 or month == 5:
-    print(seasons.get(2))
+        print(seasons.get(2))
 
 elif month == 6 or month == 7 or month == 8:
-    print(seasons.get(3))
+        print(seasons.get(3))
 
 elif month == 9 or month == 10 or month == 11:
-    print(seasons.get(4))
+        print(seasons.get(4))
 
 else:
         print("Sorry, this month does not exist. Try again")
